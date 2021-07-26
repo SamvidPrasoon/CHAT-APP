6ce16b3f3a5c415bb7adbb584602380a87b3d68f
SamvidPrasoon <76217948+SamvidPrasoon@users.noreply.github.com>
2021-07-26 22:34:45 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAZmrAiaoh7jMM0NRCkFaszY5IqC_9S1WY",
-    authDomain: "message-31f08.firebaseapp.com",
-    projectId: "message-31f08",
-    storageBucket: "message-31f08.appspot.com",
-    messagingSenderId: "923935172628",
-    appId: "1:923935172628:web:08846dc29179797c11df01",
-    measurementId: "G-GZNFDTJM2R"
+ //firebase secrets 
   
 };
 
